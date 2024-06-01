b4a03562d83fa6110e6b242c0037077db9264551
Moo. <619mistico@gmail.com>
2024-06-01 23:58:04 +0700

    add action

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import puppeteer, { Page } from "puppeteer-core";
 import html2md from "html-to-md";
 import * as fs from "fs";
 import { Command } from "commander";
-import { appendToFile, sleep } from "./utils";
+import { appendToFile, getTitle, sleep } from "./utils";
+import packageJSON from "../package.json";
 
 type TData = {
   hostURL: string;
@@ -13,9 +14,16 @@ type TData = {
 
 const program = new Command();
 
-program.version("1.0.0").description("linkedin learning kok manual");
+program
+  .version(packageJSON.version)
+  .description("linkedin learning kok manual");
+
+program.option("-t, --transcript-only", "get transcript only");
 
 program.command("run <filename>").action(async (filename) => {
+  const isTranscriptOnly = !!program.opts().transcriptOnly;
+  console.log(isTranscriptOnly);
+
   const rawData = fs.readFileSync(filename, "utf8");
   const jsonData = JSON.parse(rawData) as TData;
   const browser = await puppeteer.connect({
@@ -25,99 +33,81 @@ program.command("run <filename>").action(async (filename) => {
 
   try {
     if (jsonData.courses.length < 1) throw new Error("Courses link empty");
-
     const page = await browser.newPage();
-
     const courses = jsonData.courses;
-
     for (const course of courses) {
       console.log(course);
-
       await page.goto(course, {
         waitUntil: "load",
       });
-
       const collapsedElements = await page.$$(
         '.classroom-toc-section__toggle[aria-expanded="false"]'
       );
-
       for (const element of collapsedElements) {
         await element.click();
       }
-
       const links = await page.evaluate(() => {
         const tocItems = document.querySelectorAll(".classroom-toc-item");
-        return Array.from(tocItems)
-          .map((tocItem) => {
-            const isComplete = !!tocItem.querySelector(
-              "svg.classroom-toc-item__completed-icon"
-            );
-            if (isComplete) return null;
-            const href =
-              tocItem
-                .querySelector(".classroom-toc-item__link")
-                ?.getAttribute("href") || "";
-            return {
-              href,
-              id: tocItem.getAttribute("data-toc-content-id") || "",
-            };
-          })
-          .filter((o) => !!o);
+        const itemLinks = Array.from(tocItems).map((tocItem) => {
+          // const isComplete = !!tocItem.querySelector(
+          //   "svg.classroom-toc-item__completed-icon"
+          // );
+          // if (isComplete) return null;
+          const href =
+            tocItem
+              .querySelector(".classroom-toc-item__link")
+              ?.getAttribute("href") || "";
+          return {
+            href,
+            id: tocItem.getAttribute("data-toc-content-id") || "",
+          };
+        });
+
+        return itemLinks;
       });
 
       const linksLength = links.length;
-
       for (let i = 0; i < linksLength; i++) {
         const link = links[i]?.href;
         if (link?.includes("quiz")) {
           continue;
         }
-
         console.log(`\n🚀 ${link}\n`);
-
         const baseUrl = "https://www.linkedin.com";
         await page.goto(`${baseUrl}${link}`, {
           waitUntil: "load",
         });
-
         await sleep(2);
-
         const button = await page.waitForSelector(
           '[data-live-test-classroom-layout-tab="TRANSCRIPT"]'
         );
-
         await button?.click();
-
         page
           .waitForSelector(".classroom-transcript", { visible: true })
           .then(() => console.log("✅ got the classroom transcript container"));
 
-        console.log("⏰ Waiting video until it's end");
+        await sleep(isTranscriptOnly ? 15 : 5);
 
-        await page.waitForSelector(
-          ".classroom-toc-item--selected a .classroom-toc-item__completed-icon",
-          {
-            visible: true,
-            timeout: 0,
-          }
-        );
-
-        await sleep(5);
+        if (!isTranscriptOnly) {
+          console.log("⏰ Waiting video until it's end");
+          await page.waitForSelector(
+            ".classroom-toc-item--selected a .classroom-toc-item__completed-icon",
+            {
+              visible: true,
+              timeout: 0,
+            }
+          );
+        }
 
         const innerHTML = await page.evaluate(() => {
           const container = document.querySelector(".classroom-transcript");
           return container?.innerHTML;
         });
-
         const text = html2md(innerHTML || "", {
           skipTags: ["a"],
         });
-
         console.log(text);
-        appendToFile(
-          `./${course.replace("https://www.linkedin.com/learning/", "")}.txt`,
-          text
-        );
+        appendToFile(`./${getTitle(course)}.txt`, text);
       }
     }
   } catch (error) {
@@ -126,5 +116,4 @@ program.command("run <filename>").action(async (filename) => {
     await browser.close();
   }
 });
-
 program.parse(process.argv);
--- src/utils.ts
+++ src/utils.ts
@@ -19,6 +19,7 @@ export async function appendToFile(filePath: string, data: string) {
     if (err.code === "ENOENT") {
       await fs.promises.writeFile(filePath, data, "utf8");
       console.log(`File created and data written to: ${filePath}`);
+      return;
     } else {
       console.error(`Error appending or writing to file: ${err}`);
       throw err;
@@ -27,4 +28,16 @@ export async function appendToFile(filePath: string, data: string) {
 
   await fs.promises.appendFile(filePath, `\n\n${data}`, "utf8");
   console.log(`Data appended to: ${filePath}`);
+}
+
+export function getTitle(url: string) {
+  const regex = /\/learning\/([\w-]+)\?/;
+  const match = url.match(regex);
+
+  if (match) {
+    const extractedString = match[1];
+    return extractedString;
+  }
+
+  return url.replace("https://www.linkedin.com/learning/", "");
 }
